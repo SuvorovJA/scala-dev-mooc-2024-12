dbac8c4546d64db61bcb777c08cb49c6a9f10836
SuvorovJA <riskiroy@yandex.ru>
2025-02-01 09:44:56 +0700

    DZ3: без тестов в папке: src/test/scala/monad/for_comprehension.scala

--- src/main/scala/monad/for_comprehension.scala
+++ src/main/scala/monad/for_comprehension.scala
@@ -5,6 +5,7 @@ object for_comprehension {
   def main(args: Array[String]): Unit = {
     val result: Wrap[Int] = NonEmptyWrap(10)
 
+    println("for_comprehension:")
     val r1 = for {
       res <- result
     } yield res
@@ -24,6 +25,14 @@ object for_comprehension {
       if res > 10
     } yield res + another
     println(r3)
+
+    println("desugar:")
+    val r1desugar: Wrap[Int] = result.map((res: Int) => res)
+    println(r1desugar)
+    val r2desugar: Wrap[Int] = result.flatMap(res => anotherResult.map(another => res.+(another)) )
+    println(r2desugar)
+    val r3desugar: Wrap[Int] = result.flatMap(res => anotherResult.withFilter(another => res.>(10)).map(another => res.+(another)))
+    println(r3desugar)
   }
 
 }
